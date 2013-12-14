4251d90c7eceee7e490238368edd5059c75eff72
Cyrille <pancakos@gmail.com>
2013-12-14 15:54:01 +0100

    replacing two hidden fields by  a single one the value is a stingified JSON

--- app/assets/javascripts/edit_things.js
+++ app/assets/javascripts/edit_things.js
@@ -1,57 +1,83 @@
 //https://developers.google.com/maps/documentation/javascript/geocoding?hl=FR
-var map,
-    geocoder;
 
-function initialize() {
-    var mapOptions = {
-          center: new google.maps.LatLng(56.563955,-4.721375),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        },
-        mapContainer = document.getElementById("locationSelection");
 
-    if (mapContainer) {
-        geocoder = new google.maps.Geocoder();
-        map = new google.maps.Map(mapContainer, mapOptions);
-        google.maps.event.addListener(map, "click", function (e) { 
-            $('#latitude').val(e.latLng.lat());
-            $('#longitude').val(e.latLng.lng());
-        });         
-    }
-}
+var things = (function () {
+    var map,
+        geocoder;
 
-function geocode(elem) {
-    geocoder.geocode(
-        { 'address': elem.value },
-        function (results, status) {
-            var pos = results[0].geometry.location;
-            console.log(pos);
-            var mark = new google.maps.Marker({
-                 map: map,
-                 position: pos,
-                 visible: true
-                });
-            console.log(mark);
-            map.panTo(pos);
-            map.setZoom(17);
+    function init() {
+        var mapOptions = {
+              center: new google.maps.LatLng(56.563955,-4.721375),
+              zoom: 8,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            },
+            mapContainer = document.getElementById("locationSelection");
+
+        if (mapContainer) {
+            geocoder = new google.maps.Geocoder();
+            map = new google.maps.Map(mapContainer, mapOptions);
+            google.maps.event.addListener(map, "click", function (e) {
+                storeCoordinates(e.latLng);
+            });         
         }
-    );
-}
-          
-// $(document).ready(function () {
-//     $('#geocode').click(function() {
-//         geocode($('#thing_address').get(0));        
-//     });
-//     google.maps.event.addDomListener(window, 'load', initialize);
-// })
+        setEventHandlers();
+    }
 
+    function geocode(elem) {
+        geocoder.geocode(
+            { 'address': elem.value },
+            function (results, status) {
+                var pos = results[0].geometry.location;           
+                var mark = new google.maps.Marker({
+                     map: map,
+                     position: pos,
+                     visible: true
+                    });
+                map.panTo(pos);
+                map.setZoom(17);
+                storeCoordinates(pos);
+            }
+        );
+    }
+
+    function storeCoordinates(mapPos) {
+        var pos  = {};
+
+        pos.lat = mapPos.lat();
+        pos.lng = mapPos.lng();
+        $('#coordinates').val(JSON.stringify(pos));
+    }
+
+    function setEventHandlers() {
+        var thingPos;
+
+        $('#geocode').click(function() {
+            geocode($('#thing_address').get(0));        
+        });
+        if ($('#recorded_thing').length > 0) {
+            setMapPos(thingPos);
+        }
+    }
+
+    function setMapPos(pos) {
+        new google.maps.Marker({
+             map: map,
+             position: pos,
+             visible: true
+            });
+        map.panTo(pos);
+        map.setZoom(17);
+    }
+
+    return {
+        "init": init
+    }
+}());
+          
 var ready;
-ready = function() {
-    $('#geocode').click(function() {
-        geocode($('#thing_address').get(0));        
-    });
-    google.maps.event.addDomListener(window, 'load', initialize);
-    google.maps.event.addDomListener(window, 'page:load', initialize);
+ready = function() {    
+    google.maps.event.addDomListener(window, 'load', things.init);
+    google.maps.event.addDomListener(window, 'page:load', things.init);
 };
 
 $(document).ready(ready);
